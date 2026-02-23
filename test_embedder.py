2f2c0506327ca4de34df749b23468b53edf6b954
venkateshnvk <venkatesh.nvk23@gmail.com>
2026-02-23 13:55:04 +0530

    Delete test_embedder.py

--- test_embedder.py
+++
@@ -1,9 +0,0 @@
-from embeddings.embedder import Embedder
-
-embedder = Embedder()
-
-text = "Patient has atrial fibrillation."
-
-vector = embedder.generate_embedding(text)
-
-print("Embedding length:", len(vector))
