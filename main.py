a286ae7d8e566aa94628d692295e3bf5a9f82916
venkateshnvk <venkatesh.nvk23@gmail.com>
2026-02-23 12:46:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api.routes import router
+import os
 
 app = FastAPI(
     title="IntelliRAG",
@@ -12,3 +13,10 @@ app.include_router(router)
 @app.get("/")
 def health_check():
     return {"status": "IntelliRAG is running"}
+
+
+# Needed for Azure Web App
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
